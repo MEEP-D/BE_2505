ede84ce2db6e2931f3453c8ae16deb261ae51bdf
MEEP-D <128205570+MEEP-D@users.noreply.github.com>
2024-09-08 22:55:47 +0700

    Add files via upload
    Bai2 - Done

---
+++ BE_2505_Bai2/BE_2505_Bai2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_2505_Bai2
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            double a = Validate.ValidateData.Validation("a: ");
+            double b = Validate.ValidateData.Validation("b: ");
+            double c = Validate.ValidateData.Validation("c: ");
+            ptbac2(a, b, c);
+            Console.ReadLine();
+        }
+
+        public static void ptbac2(double a, double b, double c)
+        {
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("PT vo nghiem !!!");
+                }
+                else
+                {
+                    Console.WriteLine($"PT co nghiem duy nhat: {-c / b}");
+                }
+                return;
+            }
+            double delta = b * b - 4 * a * c;
+            double x1;
+            double x2;
+
+            if (delta > 0)
+            {
+                x1 = (double)((-b + Math.Sqrt(delta)) / (2 * a));
+                x2 = (double)((-b - Math.Sqrt(delta)) / (2 * a));
+                Console.WriteLine($"Pt co 2 nghiem la: x1 = {x1},x2 = {x2}");
+            }else if (delta == 0){
+                x1 = (-b / (2 * a));
+                Console.WriteLine("Pt co 1 nghiem x1: {0}", x1);
+            }else
+            {
+                Console.WriteLine("Phuong trinh vo nghiem !!!");
+            }
+            
+        }
+    }
+}
---
+++ BE_2505_Bai2/Validate/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Validate
+{
+    public static class ValidateData
+    {
+        public static double Validation(string message)
+        //Đây là một phương thức tĩnh (static), có nghĩa là nó có thể được gọi mà không cần tạo đối tượng của lớp ValidateData.
+        {
+            double number;
+            string input;
+
+            while (true)
+            { //Vòng lặp này sẽ tiếp tục yêu cầu người dùng nhập dữ liệu cho đến khi họ nhập một chuỗi hợp lệ.
+                //Nếu người dùng nhập không đúng (ví dụ nhập ký tự đặc biệt hoặc chữ cái),
+                //chương trình sẽ hiển thị thông báo "Repeat input number!!!" và yêu cầu nhập lại.
+                Console.WriteLine(message);
+                input = Console.ReadLine();
+                // Kiểm tra nếu chuỗi chỉ chứa các chữ số và dấu thập phân (nếu có)
+                if (Regex.IsMatch(input, @"^\d+(\.\d+)?$") && double.TryParse(input, out number))
+                    break;
+                else
+                
+                    Console.WriteLine("Repeat input number!!!");
+            }
+        return number;
+        }
+    }
+}
